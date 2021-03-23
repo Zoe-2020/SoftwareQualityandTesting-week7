420b12012b778f1b985c9409b16c79bfca27d526
Zoe-2020 <60262018+Zoe-2020@users.noreply.github.com>
2021-03-23 09:36:50 +0000

    Add project files.

---
+++ nunitLibary/SolutionTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace nunitLibary
+{
+    [TestFixture]
+    class SolutionTest
+    {
+        CustomMath cm = new CustomMath();
+        [Test]
+        
+        public void Add()
+        {
+            Assert.AreEqual(20, cm.multiply(5, 4));
+        }
+        
+        [Test]
+        public void div()
+        {
+            Assert.AreEqual(true, cm.divisible(3, 1, 3));
+            Assert.AreEqual(true, cm.divisible(12, 2, 6));
+            Assert.AreEqual(false, cm.divisible(100, 5, 3));
+            Assert.AreEqual(false, cm.divisible(12, 5, 3));
+            
+        }
+
+        [Test]
+        public void watermelonsDiv()
+        {
+            Assert.AreEqual(true, cm.Q2Div(2, 6));
+            Assert.AreEqual(true, cm.Q2Div(4, 4));
+            Assert.AreEqual(false, cm.Q2Div(3, 5));
+            Assert.AreEqual(false, cm.Q2Div(7, 1));
+        }
+    }
+}
